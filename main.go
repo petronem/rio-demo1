7c284706c06759f1e081e467a09aa8951e900131
petronem <matt.petrone@carii.com>
2021-04-06 22:23:47 -0400

    v6

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm rio-demo1 running in Rio v5")
+	fmt.Fprintln(w, "Hi there, I'm rio-demo1 running in Rio v6")
 }
 
 func main() {
